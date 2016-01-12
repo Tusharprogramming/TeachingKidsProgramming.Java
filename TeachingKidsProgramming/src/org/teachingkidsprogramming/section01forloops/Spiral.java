952fe30a3870fc5e9033d7a7d9ad1cb344b56e88
Tusharprogramming <tushar.bhagawatula@gmail.com>
2016-01-12 13:11:36 -0600

    Started Spiral

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section01forloops/Spiral.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section01forloops/Spiral.java
@@ -1,5 +1,8 @@
 package org.teachingkidsprogramming.section01forloops;
 
+import org.teachingextensions.logo.Tortoise;
+import org.teachingextensions.logo.utils.ColorUtils.ColorWheel;
+import org.teachingextensions.logo.utils.ColorUtils.PenColors;
 import org.teachingextensions.logo.utils.EventUtils.MessageBox;
 
 public class Spiral
@@ -7,16 +10,24 @@ public class Spiral
   public static void main(String[] args)
   {
     //    Show the tortoise --#1
+    Tortoise.show();
     //    Make the tortoise go as fast as possible --#4
+    Tortoise.setSpeed(10);
     //    Add Blue Violet to the Color Wheel --#7
+    ColorWheel.addColor(PenColors.Purples.BlueViolet);
     //    Add Violet to the Color Wheel --#8
     //    Add Purple to the Color Wheel --#9
     //    Do the following 75 times --#3.1
     try
     {
-      //         Change the pen color of the line the tortoise draws the next color on the Color Wheel --#6
-      //         Move the tortoise 5 times the current line number you are drawing --#5
-      //         Turn the tortoise 1/3 of 360 degrees to the right --#2
+      for (int i = 0; i < 75; i++)
+      {
+        //         Change the pen color of the line the tortoise draws the next color on the Color Wheel --#6
+        //         Move the tortoise 5 times the current line number you are drawing --#5
+        Tortoise.move(i);
+        //         Turn the tortoise 1/3 of 360 degrees to the right --#2
+        Tortoise.turn(120);
+      }
     }
     catch (RuntimeException re)
     {
