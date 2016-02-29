6335aae8718956e421215836ee3fd85bd73b701c
Tusharprogramming <tushar.bhagawatula@gmail.com>
2016-02-29 11:34:00 -0600

    started

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section06modelviewcontroller/AdLibs.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section06modelviewcontroller/AdLibs.java
@@ -1,5 +1,7 @@
 package org.teachingkidsprogramming.section06modelviewcontroller;
 
+import org.teachingextensions.logo.utils.EventUtils.MessageBox;
+
 public class AdLibs
 {
   public static void main(String[] args)
@@ -8,9 +10,11 @@ public class AdLibs
     //  Ask the user to enter a verb ending in '-ed', save it as currentEdVerb --#4
     //  Ask the user to enter a body part, save it as currentBodyPart --#6
     //  Set the value of the currentStory to the word "Today " --#1.2
+    String currentStory = "Today";
     //  Add the words "I woke " + currentAdverb + ". " to the currentStory --#3
     //  Add the words '"Then I " + currentEdVerb + " " to the currentStory --#5
     //  Add the words "my " + currentBodyPart + ". " to the current story --#7
     //  Show the currentStory in a message box as a message --#1.1
+    MessageBox.showMessage(currentStory);
   }
 }
