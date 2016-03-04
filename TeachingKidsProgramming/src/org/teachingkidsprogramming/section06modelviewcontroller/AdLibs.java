4977fe5d47969fd496f6dc96f0b30e208493d6bf
Tusharprogramming <tushar.bhagawatula@gmail.com>
2016-03-04 12:05:38 -0600

    tushar changes

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section06modelviewcontroller/AdLibs.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section06modelviewcontroller/AdLibs.java
@@ -9,9 +9,17 @@ public class AdLibs
     //  Ask the user to enter an adverb, save it as currentAdverb --#2
     String currentAdverb = MessageBox.askForTextInput("Enter an Adverb");
     //  Ask the user to enter a verb ending in '-ed', save it as currentEdVerb --#4
-    String currentEdVerb = MessageBox.askForTextInput("Enter a Verb in Present Tense");
+    String currentEdVerb = MessageBox.askForTextInput("Enter a Verb in Perfect Tense");
     //  Ask the user to enter a body part, save it as currentBodyPart --#6
     String currentBodyPart = MessageBox.askForTextInput("Enter a Body Part");
+    // Insert more stuff
+    String currentinterjection = MessageBox.askForTextInput("Enter an Interjection");
+    //
+    String anotherperfveb = MessageBox.askForTextInput("Enter Verb in Perfect Tense");
+    //
+    String noun = MessageBox.askForTextInput("Enter a Noun");
+    String presverb = MessageBox.askForTextInput("Enter Verb in Perfect Tense");
+    String adverb = MessageBox.askForTextInput("Enter an Adverb (Capitalize this)");
     //  Set the value of the currentStory to the word "Today " --#1.2
     String currentStory = "Today ";
     //  Add the words "I woke " + currentAdverb + ". " to the currentStory --#3
@@ -20,6 +28,11 @@ public class AdLibs
     currentStory = currentStory + "Then I " + currentEdVerb + " ";
     //  Add the words "my " + currentBodyPart + ". " to the current story --#7
     currentStory = currentStory + "my " + currentBodyPart + ".";
+    //moar shtuph
+    currentStory = currentStory + " " + "'" + currentinterjection + "!" + "'";
+    //
+    currentStory = currentStory + " I " + anotherperfveb + ".";
+    currentStory = currentStory + " " + adverb + ", the " + noun + " " + presverb;
     //  Show the currentStory in a messa as a message --#1.1
     MessageBox.showMessage(currentStory);
   }
