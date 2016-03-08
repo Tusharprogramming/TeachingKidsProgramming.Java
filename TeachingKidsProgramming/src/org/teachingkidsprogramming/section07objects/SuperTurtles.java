39ae9bffab78b0bc728b60c21876809c8714f8a0
Tusharprogramming <tushar.bhagawatula@gmail.com>
2016-03-08 13:15:53 -0600

    ydyjmtgmkh

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section07objects/SuperTurtles.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section07objects/SuperTurtles.java
@@ -1,6 +1,7 @@
 package org.teachingkidsprogramming.section07objects;
 
 import org.teachingextensions.WindowUtils.MultiTurtleWindow;
+import org.teachingextensions.logo.Turtle;
 
 public class SuperTurtles
 {
@@ -16,23 +17,38 @@ public class SuperTurtles
   private void showSomeTurtles()
   {
     //  makeSpeedyTurtle (recipe below) --#2.0
-    //  ------------- Recipe for makeSpeedyTurtle --#1.0
-    //        Create a new speedyTurtle instance  
-    //        Add your speedyTurtle to your MultiTurtleWindow
-    //        Set the speed of your speedyTurtle to the fastest possible    
-    //        Have your speedyTurtle draw a triangle with 100 pixel sides
-    //  ------------- End of makeSpeedyTurtle --#1.1 
+    makeSpeedyTortose();
     //  makeSlowTurtle (recipe below) --#4.0
-    //  ------------- Recipe for makeSlowTurtle --#3.0
-    //        Create a new slowTurtle instance 
-    //        Add your slowTurtle to your MultiTurtleWindow
-    //        Have your slowTurtle draw a upside down triangle with 50 pixel sides
-    //  ------------- End of makeSlowTurtle --#3.1
-    //  makeCrazyTurtle (recipe below) --#6.0
-    //  ------------- Recipe for makeCrazyTurtle --#5.1
+    makeSlowTortose();
     //        Create a new crazyTurtle instance  
     //        Add your crazyTurtle to your MultiTurtleWindow
     //        Have your crazyTurtle draw a 55 pixel long lightning bolt
     //  ------------- End of makeCrazyTurtle --#5.2
+  }
+  private void makeSlowTortose()
+  {
+    //  ------------- Recipe for makeSlowTurtle --#3.0
+    //        Create a new slowTurtle instance
+    Turtle slowtortose = new Turtle();
+    //        Add your slowTurtle to your MultiTurtleWindow
+    mtw.addAndShowTurtle(slowtortose);
+    //        Have your slowTurtle draw a upside down triangle with 50 pixel sides
+    slowtortose.drawTriangle(-50);
+    //  ------------- End of makeSlowTurtle --#3.1
+    //  makeCrazyTurtle (recipe below) --#6.0
+    //  ------------- Recipe for makeCrazyTurtle --#5.1
+  }
+  private void makeSpeedyTortose()
+  {
+    //  ------------- Recipe for makeSpeedyTurtle --#1.0
+    //        Create a new speedyTurtle instance 
+    Turtle speedytortose = new Turtle();
+    //        Add your speedyTurtle to your MultiTurtleWindow
+    mtw.addAndShowTurtle(speedytortose);
+    //        Set the speed of your speedyTurtle to the fastest possible 
+    speedytortose.setSpeed(10);
+    //        Have your speedyTurtle draw a triangle with 100 pixel sides
+    speedytortose.drawTriangle(100);
+    //  ------------- End of makeSpeedyTurtle --#1.1 
   }
 }
