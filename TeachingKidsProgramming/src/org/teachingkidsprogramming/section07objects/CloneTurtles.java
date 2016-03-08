e9d85fd34a4e6000f88ae35c88705f5de208fce1
Tusharprogramming <tushar.bhagawatula@gmail.com>
2016-03-08 12:38:13 -0600

    1-6.3

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section07objects/CloneTurtles.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section07objects/CloneTurtles.java
@@ -1,10 +1,15 @@
 package org.teachingkidsprogramming.section07objects;
 
+import java.util.ArrayList;
+
+import org.teachingextensions.WindowUtils.MultiTurtleWindow;
+import org.teachingextensions.logo.Turtle;
+
 public class CloneTurtles
 {
   // Uncomment the two lines of code below to create a container and a window for your turtles --#1
-  // public ArrayList<Turtle> turtles = new ArrayList<Turtle>();
-  // public MultiTurtleWindow mtw     = new MultiTurtleWindow();
+  public ArrayList<Turtle> turtles = new ArrayList<Turtle>();
+  public MultiTurtleWindow mtw     = new MultiTurtleWindow();
   //
   public CloneTurtles()
   {
@@ -14,22 +19,36 @@ public class CloneTurtles
   private void showSomeTurtles()
   {
     // Use a FOR loop to add your three turtles --#4.1
-    //      Create a turtle instance  --#2.1
-    //      Add your turtles to your turtle container --#2.2
+    for (int i = 0; i < 3; i++)
+    {
+      //      Create a turtle instance  --#2.1
+      Turtle testsubject = new Turtle();
+      //      Add your turtles to your turtle container --#2.2
+      turtles.add(testsubject);
+    }
     // Repeat --#4.2
     //
     // Create a variable to count the number of turtles in the container --#3.1
+    int NumOfTortoses = turtles.size();
     // Uncomment to Show the number of turtles in the container on the window --#3.2
     // new Text("There are " + numberOfTurtles + " turtles in the turtle container").setTopLeft(50, 50).setPenColor(PenColors.Yellows.Gold).addTo(mtw);
     //
     // Use a FOREACH loop to add your turtles to your window --#5.1
-    //      Add and Show your turtles on your window --#5.3
-    // Repeat --#5.2
+    for (Turtle turtle : turtles)
+    {
+      //      Add and Show your turtles on your window --#5.3
+      mtw.addAndShowTurtle(turtle);
+      // Repeat --#5.2
+    }
     //
     // Use a FOR loop to teleport all of your turtles around your window  --#6.1
-    //      Get your turtle's current position and then set the X position to i*100 + 350 --#6.3
-    //      Get your turtle's current position and then set the Y position to i*100 + 100 --#6.4
-    // Repeat --#6.2
+    for (int i = 0; i < 3; i++)
+    {
+      //      Get your turtle's current position and then set the X position to i*100 + 350 --#6.3
+      turtles.get(i).setX(i * 100 + 350);
+      //      Get your turtle's current position and then set the Y position to i*100 + 100 --#6.4
+      // Repeat --#6.2 
+    }
     //
     // Use a FOREACH loop to set values for all of your turtles --#7.1
     //      Set the pen width to 10 pixels -- #7.3
